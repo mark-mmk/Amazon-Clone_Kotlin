78bc66c1f5352e8f29e78532f6c36fd119d1b7ea
mark <markmekhail1111@gmail.com>
2024-05-26 11:11:27 +0300

    six add

---
+++ app/src/main/java/com/example/amazon/Adapter/CategoryItem.kt
@@ -0,0 +1,5 @@
+package com.example.amazon.Adapter
+
+data class CategoryItem(
+    val phone:String,
+)
---
+++ app/src/main/java/com/example/amazon/test/carttest.kt
@@ -0,0 +1,11 @@
+package com.example.amazon.test
+
+data class carttest(
+    val pid:String? = null,
+    val uid:String? = null,
+    val imageUrl: Int? = null,
+    val name: String? = null,
+    val price: String? = null,
+    val size: String? = null,
+    val quantity: Int? = null
+)
